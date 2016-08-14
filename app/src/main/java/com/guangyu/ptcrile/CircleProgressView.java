247e8031823f49dd7b2c1b1c70939c67f758814e
lobster002 <752996878@qq.com>
2016-08-14 18:20:32 +0800

    调整代码结构

--- app/src/main/java/com/guangyu/ptcrile/CircleProgressView.java
+++ app/src/main/java/com/guangyu/ptcrile/CircleProgressView.java
@@ -10,87 +10,57 @@ import android.graphics.RectF;
 import android.os.Build;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 /**
- * 如果觉得进度太快请设置{@link #EVERY_INVALIDATE_INTERVAL} 单位是毫秒 <br>
  * 要修改文字内容请调用{@link #setCenterText(String)} <br>
- * <code>Activity onDestroy</code>的时候,请调用{@link #stopAnim()} ,防止内存泄露<br>
+ * 修改进度 调用{@link #setCurrentProgress(long)} 单位为毫秒  取值为 0 ~ 30 * 1000
+ * <code>onDestroy</code>的时候,请调用{@link #stopAnim()} ,防止内存泄露<br>
+ * 关于暂停刷新界面 {@link #stopAnim()}  返回当前的 进度 progress  需要保存进度  调用 {@link #startAnim(long)} 参数为返回的进度值
  */
 public class CircleProgressView extends View {
-    /**
-     * 每次自动刷新时间,可以自己修改(请暂不要自己修改)
-     */
-    private final long EVERY_INVALIDATE_INTERVAL = 10L;
-    /**
-     * 完成一圈进度时间(请暂不要自己修改)
-     */
-    private final long COMPLETE_PROGRESS_TIME = 30 * 1000L;
-    /**
-     * 旋转总的进度
-     */
-    private final float COMPLETE_PROGRESS_DEGREE = 360F;
-    /**
-     * 最开始的弧度
-     */
-    private final float DEFAULT_PROGRESS_DEGREE = -90F;
-    /**
-     * 圆环画笔
-     */
-    private Paint mRingPaint;
-    /**
-     * 圆环画笔
-     */
-    private Paint mBgCirclePaint;
-    /**
-     * 圆环上面小圆环画笔
-     */
-    private Paint mSmallCirclePaint;
-    /**
-     * 圆环上面文字画笔
-     */
-    private Paint mSmallTextPaint;
-    /**
-     * 中间文字画笔
-     */
 
-    private Paint insideCirclePaint;
-    private Paint mTextPaint;
-    private volatile String mTextStr;
-    private float mTextSize;
-    private float mSmallTextSize;
-    private float mSmallCircleRadius;
-    private float mRingStrokeWidth;
+    private final long EVERY_INVALIDATE_INTERVAL = 10L;// 每次自动刷新时间,可以自己修改(请暂不要自己修改)
+
+    private final long COMPLETE_PROGRESS_TIME = 30 * 1000L;//完成一圈进度时间(请暂不要自己修改)
+
+    private final float DEFAULT_PROGRESS_DEGREE = -90F;//最开始的弧度
+
+    private Paint mRingPaint;//圆环画笔
+    private Paint mBgCirclePaint;//背景圆
+    private Paint mSmallCirclePaint;//圆环上面小圆环画笔
+    private Paint mSmallTextPaint;//圆环上面文字画笔
+    private Paint insideCirclePaint;//园内环画笔
+    private Paint mTextPaint;//中间文字画笔
+
+    private volatile String mTextStr = " ";//中间文本
+
+    private float mTextSize;//中间文本字体大小
+    private float mSmallTextSize;//小圆文字字体大小
+    private float mSmallCircleRadius;//小圆半径
+    private float mRingStrokeWidth;//园内环宽
+
     private RectF insideRectF;
     private float sweepAngle;//偏转角度
 
-    //控件中心坐标
-    private int centerX = 0;
-    private int centerY = 0;
+    private int centerX = 0;//中心点X值
+    private int centerY = 0;//中心点Y值
 
     private volatile int currentColor = 0;//当前进度条颜色
 
-
     private int deltaCircleValue = 12;//内外圆半径差
 
-    private float radius;
-    /**
-     * 当前已经刷新的次数
-     */
+    private float radius;//进度半径
+
     private float mTextX;
     private float mTextY;
 
-    private boolean mShouldStopAnim;
+    private volatile long startTime = 0l;//本次绘制开始时间
+    private volatile long theStartTime = 0l;//本次进度条开始时间
 
+    private int bgColor = Color.parseColor("#33000000");//背景颜色
 
-    private OnDrawCircleListener listener = null;
-
-    private volatile long startTime = 0l;
-    private volatile long theStartTime = 0l;
-
-    private int bgColor = Color.parseColor("#33000000");
-
+    //文本字体大小  单位sp
     private float SMALL_TEXT_SIZE = 15.0f;
     private float CENTER_TEXT_SIZE = 55.0f;
 
@@ -98,26 +68,26 @@ public class CircleProgressView extends View {
 
     public CircleProgressView(Context context) {
         super(context);
-        init(context, null);
+        init();
     }
 
     public CircleProgressView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context, attrs);
+        init();
     }
 
     public CircleProgressView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context, attrs);
+        init();
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public CircleProgressView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        init(context, attrs);
+        init();
     }
 
-    private void init(Context context, AttributeSet attrs) {
+    private void init() {
         mTextSize = sp2px(CENTER_TEXT_SIZE);
         mSmallTextSize = sp2px(SMALL_TEXT_SIZE);
         mRingStrokeWidth = dip2px(10);
@@ -148,7 +118,6 @@ public class CircleProgressView extends View {
         mSmallTextPaint.setColor(Color.WHITE);
 
         startTime = theStartTime = System.currentTimeMillis();
-
     }
 
     @Override
@@ -178,24 +147,35 @@ public class CircleProgressView extends View {
         }
     }
 
-    private void doAfter() {//绘制完毕之后 其他的逻辑
-//        long endTime = System.currentTimeMillis();
-//        Log.e("Tag", String.valueOf((endTime - startTime) / 1000));
-        if (sweepAngle > 360.0f) {
-            if (null != listener) {
-                listener.callback();
-            }
-            synchronized (lock) {
-                theStartTime = startTime = System.currentTimeMillis();
-            }
+    private void drawBg(Canvas canvas) {
+        canvas.drawCircle(centerX, centerY, radius, mBgCirclePaint);
+    }
+
+    private void drawCircle(Canvas canvas) {
+        long deltaTime = startTime - theStartTime;
+        if (deltaTime > COMPLETE_PROGRESS_TIME) {
+            deltaTime %= COMPLETE_PROGRESS_TIME;
         }
-        if (!mShouldStopAnim) {
-            postInvalidateDelayed(EVERY_INVALIDATE_INTERVAL);
+        sweepAngle = (float) (deltaTime * 0.012);//偏转角度
+        float fraction = sweepAngle / 360.0f;
+        currentColor = getCurrentColor(fraction);
+        mRingPaint.setColor(currentColor);
+
+        canvas.drawArc(insideRectF, DEFAULT_PROGRESS_DEGREE, 360.0f, false, insideCirclePaint);
+        canvas.drawArc(insideRectF, DEFAULT_PROGRESS_DEGREE, sweepAngle, false, mRingPaint);
+    }
+
+    private void drawText(Canvas canvas) {
+        if (!TextUtils.isEmpty(mTextStr)) {
+            Rect rect = new Rect();
+            mTextPaint.getTextBounds(mTextStr, 0, mTextStr.length(), rect);
+            mTextX = centerX - rect.width() / 2F;
+            mTextY = centerY + rect.height() / 2F;
+            canvas.drawText(mTextStr, mTextX, mTextY, mTextPaint);
         }
     }
 
     private void drawDot(Canvas canvas) {
-
         if (sweepAngle > 300.0f) {
             int index = (int) ((360 - sweepAngle) / 12 + 1);
             // 第四象限(先转换为角度,然后根据圆里面的关系去换算为坐标)
@@ -228,35 +208,19 @@ public class CircleProgressView extends View {
 
     }
 
-    private void drawText(Canvas canvas) {
-
-        if (!TextUtils.isEmpty(mTextStr)) {
-            Rect rect = new Rect();
-            mTextPaint.getTextBounds(mTextStr, 0, mTextStr.length(), rect);
-            mTextX = centerX - rect.width() / 2F;
-            mTextY = centerY + rect.height() / 2F;
-            canvas.drawText(mTextStr, mTextX, mTextY, mTextPaint);
+    private void doAfter() {//绘制完毕之后 其他的逻辑
+        if (sweepAngle > 360.0f) {
+            if (null != listener) {
+                listener.callback();
+            }
+            synchronized (lock) {
+                theStartTime = startTime = System.currentTimeMillis();
+            }
+        }
+        if (!isAnimStop) {
+            postInvalidateDelayed(EVERY_INVALIDATE_INTERVAL);
         }
     }
-
-    private void drawCircle(Canvas canvas) {
-        long deltaTime = startTime - theStartTime;
-        if (deltaTime > COMPLETE_PROGRESS_TIME) {
-            deltaTime %= COMPLETE_PROGRESS_TIME;
-        }
-        sweepAngle = (float) (deltaTime * 0.012);//偏转角度
-        float fraction = sweepAngle / COMPLETE_PROGRESS_DEGREE;
-        currentColor = getCurrentColor(fraction);
-        mRingPaint.setColor(currentColor);
-
-        canvas.drawArc(insideRectF, DEFAULT_PROGRESS_DEGREE, 360.0f, false, insideCirclePaint);
-        canvas.drawArc(insideRectF, DEFAULT_PROGRESS_DEGREE, sweepAngle, false, mRingPaint);
-    }
-
-    private void drawBg(Canvas canvas) {
-        canvas.drawCircle(centerX, centerY, radius, mBgCirclePaint);
-    }
-
 
     public void setCurrentProgress(long value) {
         if (value < 0) {
@@ -271,71 +235,62 @@ public class CircleProgressView extends View {
         }
     }
 
+    private boolean isAnimStop = false;//状态标记位
 
     public boolean isAnimStopped() {//判断是否 停止更新
-        return mShouldStopAnim;
+        return isAnimStop;
     }
 
     public void startAnim() {
-        mShouldStopAnim = false;
+        isAnimStop = false;
         postInvalidateDelayed(EVERY_INVALIDATE_INTERVAL);
     }
 
 
     public void startAnim(long currentProgress) {
-        mShouldStopAnim = false;
+        isAnimStop = false;
         setCurrentProgress(currentProgress);
         postInvalidateDelayed(EVERY_INVALIDATE_INTERVAL);
     }
 
     public long stopAnim() {
-        mShouldStopAnim = true;
+        isAnimStop = true;
         long currentTime = System.currentTimeMillis();
         return currentTime - theStartTime;
     }
 
-
-    /**
-     * 这里根据需要去改变中间需要显示的字符串
-     *
-     * @param str
-     */
+    /*
+    * 设置中间文本显示内容
+    * */
     public void setCenterText(String str) {
         mTextStr = str;
     }
 
-    /**
-     * 将dip或dp值转换为px值，保证尺寸大小不变
-     *
-     * @param dipValue
-     * @return
-     */
+    //dp转px
     public int dip2px(float dipValue) {
         final float scale = getContext().getResources().getDisplayMetrics().density;
         return (int) (dipValue * scale + 0.5f);
     }
 
-    /**
-     * 将sp值转换为px值，保证文字大小不变
-     *
-     * @param spValue
-     * @return
-     */
+    //sp转px
     public int sp2px(float spValue) {
         final float fontScale = getContext().getResources().getDisplayMetrics().scaledDensity;
         return (int) (spValue * fontScale + 0.5f);
     }
 
+    //因为该方法调用频繁，变量声明在这里，减少每次调用时，为临时变量分配内存次数
+    int startA, startR, startG, startB, endA, endR, endG, endB;
+    //颜色渐变算法 写的还不错
     public int evaluate(float fraction, int startValue, int endValue) {
-        int startA = (startValue >> 24) & 0xff;
-        int startR = (startValue >> 16) & 0xff;
-        int startG = (startValue >> 8) & 0xff;
-        int startB = startValue & 0xff;
+        startA = (startValue >> 24) & 0xff;
+        startR = (startValue >> 16) & 0xff;
+        startG = (startValue >> 8) & 0xff;
+        startB = startValue & 0xff;
 
-        int endA = (endValue >> 24) & 0xff;
-        int endR = (endValue >> 16) & 0xff;
-        int endG = (endValue >> 8) & 0xff;
-        int endB = endValue & 0xff;
+        endA = (endValue >> 24) & 0xff;
+        endR = (endValue >> 16) & 0xff;
+        endG = (endValue >> 8) & 0xff;
+        endB = endValue & 0xff;
 
         return ((startA + (int) (fraction * (endA - startA))) << 24) |
                 ((startR + (int) (fraction * (endR - startR))) << 16) |
@@ -348,6 +303,7 @@ public class CircleProgressView extends View {
     private final int COLOR_MEDUIM = Color.parseColor("#ffdd3c");
     private final int COLOR_END = Color.parseColor("#ff3c61");
 
+    //分段渐变 返回当前进度下 对应的颜色
     private int getCurrentColor(float progress) {
         if (progress <= 0.5f) {
             return COLOR_BEGIN;
@@ -358,6 +314,7 @@ public class CircleProgressView extends View {
         }
     }
 
+    private OnDrawCircleListener listener = null;   //回调接口
 
     public void setOnDrawCircleListener(OnDrawCircleListener onDrawCircleListener) {
         listener = onDrawCircleListener;
