65f43068bc799423d01a8415470b0b7e3d8ad72d
lobster002 <752996878@qq.com>
2016-09-14 19:19:56 +0800

    调整文本居中显示，优化局部变量

--- app/src/main/java/com/guangyu/ptcrile/CircleProgressView.java
+++ app/src/main/java/com/guangyu/ptcrile/CircleProgressView.java
@@ -1,4 +1,4 @@
-package com.guangyu.ptcrile;
+package sky.com.myapplication;
 
 import android.annotation.TargetApi;
 import android.content.Context;
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.Typeface;
 import android.os.Build;
 import android.text.TextUtils;
 import android.util.AttributeSet;
@@ -52,9 +53,6 @@ public class CircleProgressView extends View {
 
     private float radius;//进度半径
 
-    private float mTextX;
-    private float mTextY;
-
     private volatile long startTime = 0l;//本次绘制开始时间
     private volatile long theStartTime = 0l;//本次进度条开始时间
 
@@ -62,7 +60,9 @@ public class CircleProgressView extends View {
 
     //文本字体大小  单位sp
     private float SMALL_TEXT_SIZE = 15.0f;
-    private float CENTER_TEXT_SIZE = 55.0f;
+    //    private float CENTER_TEXT_SIZE = 55.0f;
+    private float CENTER_TEXT_SIZE = 40.0f;
+    private Paint.FontMetrics fontMetrics = null;
 
     private Object lock = new Object();
 
@@ -113,10 +113,14 @@ public class CircleProgressView extends View {
         mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mTextPaint.setTextSize(mTextSize);
         mTextPaint.setColor(Color.WHITE);
+        mTextPaint.setStyle(Paint.Style.STROKE);
 
         mSmallTextPaint = new Paint(mTextPaint);
         mSmallTextPaint.setTextSize(mSmallTextSize);
         mSmallTextPaint.setColor(Color.WHITE);
+
+        mTextPaint.setTypeface(Typeface.DEFAULT);//默认字体
+        mTextPaint.setTextAlign(Paint.Align.LEFT);
 
         startTime = theStartTime = System.currentTimeMillis();
     }
@@ -132,6 +136,8 @@ public class CircleProgressView extends View {
     }
 
 
+    private long deltaTime = 0;
+    private float fraction = 0.0f;
     @Override
     protected void onDraw(Canvas canvas) {
         doBefore();//绘制之前
@@ -153,9 +159,9 @@ public class CircleProgressView extends View {
     }
 
     private void drawCircle(Canvas canvas) {
-        long deltaTime = startTime - theStartTime;
+        deltaTime = startTime - theStartTime;
         sweepAngle = (float) (deltaTime * 0.012);//偏转角度
-        float fraction = sweepAngle / 360.0f;
+        fraction = sweepAngle / 360.0f;
         currentColor = getCurrentColor(fraction);
         mRingPaint.setColor(currentColor);
 
@@ -163,29 +169,35 @@ public class CircleProgressView extends View {
         canvas.drawArc(insideRectF, DEFAULT_PROGRESS_DEGREE, sweepAngle, false, mRingPaint);
     }
 
+    private int baseLine = 0;
     private void drawText(Canvas canvas) {
         if (!TextUtils.isEmpty(mTextStr)) {
-            Rect rect = new Rect();
-            mTextPaint.getTextBounds(mTextStr, 0, mTextStr.length(), rect);
-            mTextX = centerX - rect.width() / 2F;
-            mTextY = centerY + rect.height() / 2F;
-            canvas.drawText(mTextStr, mTextX, mTextY, mTextPaint);
+            fontMetrics = mTextPaint.getFontMetrics();
+            baseLine = (int) (((centerY << 1) - fontMetrics.bottom + fontMetrics.top) / 2 - fontMetrics.top);
+            canvas.drawText(mTextStr, centerX - mTextPaint.measureText(mTextStr) / 2, baseLine, mTextPaint);
         }
     }
 
+    private int index = 0;
+    private double tmpAngle = 0;
+    private double sin = 0;
+    private double cos = 0;
+    private float x1 = 0;
+    private float y1 = 0;
+    private String text = " ";
     private void drawDot(Canvas canvas) {
         if (sweepAngle > 300.0f) {
-            int index = (int) ((360 - sweepAngle) / 12 + 1);
+            index = (int) ((360 - sweepAngle) / 12 + 1);
             // 第四象限(先转换为角度,然后根据圆里面的关系去换算为坐标)
-            double angle = Math.toRadians(sweepAngle - 270f);
-            double sin = Math.sin(angle);
-            double cos = Math.cos(angle);
-            float x1 = (float) (centerX - (radius - dip2px(deltaCircleValue)) * cos);
-            float y1 = (float) (centerY - (radius - dip2px(deltaCircleValue)) * sin);
+            tmpAngle = Math.toRadians(sweepAngle - 270f);
+            sin = Math.sin(tmpAngle);
+            cos = Math.cos(tmpAngle);
+            x1 = (float) (centerX - (radius - dip2px(deltaCircleValue)) * cos);
+            y1 = (float) (centerY - (radius - dip2px(deltaCircleValue)) * sin);
             mSmallCirclePaint.setColor(currentColor);
 
             // 调节位置,  是为了让文字居中
-            String text = String.valueOf(index);
+            text = String.valueOf(index);
             if (sweepAngle > 348.0f) {//1
                 float scale = mSmallCircleRadius - dip2px((sweepAngle - 348.0f) / 2);
                 canvas.drawCircle(x1, y1, scale, mSmallCirclePaint);
@@ -219,6 +231,7 @@ public class CircleProgressView extends View {
             postInvalidateDelayed(POST_WAIT_TIME);
         }
     }
+
 
     public void setCurrentProgress(long value) {
         if (value < 0) {
@@ -261,7 +274,7 @@ public class CircleProgressView extends View {
     * 设置中间文本显示内容
     * */
     public void setCenterText(String str) {
-        mTextStr = str;
+        mTextStr = str.trim();
     }
 
     //dp转px
@@ -277,8 +290,8 @@ public class CircleProgressView extends View {
     }
 
     //因为该方法调用频繁，变量声明在这里，减少每次调用时，为临时变量分配内存次数
-    int startA, startR, startG, startB, endA, endR, endG, endB;
 
+    int startA, startR, startG, startB, endA, endR, endG, endB;
     //颜色渐变算法 写的还不错
     public int evaluate(float fraction, int startValue, int endValue) {
         startA = (startValue >> 24) & 0xff;
@@ -301,7 +314,6 @@ public class CircleProgressView extends View {
     private final int COLOR_BEGIN = Color.parseColor("#00ffa8");
     private final int COLOR_MEDUIM = Color.parseColor("#ffdd3c");
     private final int COLOR_END = Color.parseColor("#ff3c61");
-
     //分段渐变 返回当前进度下 对应的颜色
     private int getCurrentColor(float progress) {
         if (progress <= 0.5f) {
